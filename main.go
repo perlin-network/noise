90ae25145df88efbc2def6d4f4887bbf6a80d3d6
doug <doug@perlin.net>
2018-06-21 00:32:43 +0800

    Ad a dedup check for the peers address list

--- main.go
+++ main.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"strings"
+
 	"github.com/perlin-network/noise/crypto"
 	"github.com/perlin-network/noise/log"
 	"github.com/perlin-network/noise/network"
-	"strings"
 )
+
+//
+func filterPeers(host string, port int, peers []string) []string {
+	currAddress := fmt.Sprintf("%s:%d", host, port)
+	peersLen := len(peers)
+	filtered := make([]string, peersLen)
+	visitedSet := make(map[string]struct{}, peersLen)
+	for _, peer := range peers {
+		if peer != currAddress {
+			// remove if it is the current host and port
+			if _, ok := visitedSet[peer]; !ok {
+				// remove if it is a duplicate in the list
+				filtered = append(filtered, peer)
+				visitedSet[peer] = struct{}{}
+			}
+		}
+	}
+	return filtered
+}
 
 func main() {
 	portFlag := flag.Int("port", 3000, "port to listen to")
+	hostFlag := flag.String("host", "localhost", "host to listen to")
 	peersFlag := flag.String("peers", "", "peers to connect to")
 	flag.Parse()
 
 	port := *portFlag
+	host := *hostFlag
 	peers := strings.Split(*peersFlag, ",")
+	peers = filterPeers(host, port, peers)
 
 	keys := crypto.RandomKeyPair()
 
 	log.Print("Private Key: " + keys.PrivateKeyHex())
 	log.Print("Public Key: " + keys.PublicKeyHex())
 
-	net := network.CreateNetwork(keys, "localhost", port)
+	net := network.CreateNetwork(keys, host, port)
 
 	net.Listen()
 
-	if len(*peersFlag) > 0 {
+	if len(peers) > 0 {
 		net.Bootstrap(peers...)
 	}
 
