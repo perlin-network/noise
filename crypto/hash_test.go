327b70a3da32a9f634ff022b37c2e7e945b39cfb
abbychau <abbychau@gmail.com>
2018-06-26 02:45:38 +0900

    remove newline characters and capital letters

--- crypto/hash_test.go
+++ crypto/hash_test.go
@@ -13,10 +13,10 @@ func TestHash(t *testing.T) {
 	n, ok := n.SetString("89391711502145780362310349925943903708999319576398061903082165979787487688967", 10)
 	if ok {
 		if n.String() != r.String() {
-			t.Fatalf("Hash error\n%v \n%v \n", r, n)
+			t.Fatalf("hash error %v %v", r, n)
 		}
 	} else {
-		t.Fatal("Big Int error")
+		t.Fatal("big int error")
 	}
 }
 
@@ -24,6 +24,6 @@ func TestHashBytes(t *testing.T) {
 	r := HashBytes([]byte("123"))
 	n := []byte{245, 214, 123, 174, 115, 176, 225, 13, 13, 253, 48, 67, 179, 244, 241, 0, 173, 160, 20, 197, 195, 123, 213, 206, 151, 129, 59, 19, 245, 171, 43, 207}
 	if !bytes.Equal(n, r) {
-		t.Fatalf("Hash error : \n%v \n%v \n", r, n)
+		t.Fatalf("hash error : %v %v", r, n)
 	}
 }
--- peer/identity_test.go
+++ peer/identity_test.go
@@ -19,35 +19,35 @@ var (
 func TestIDEqual(t *testing.T) {
 
 	if !bytes.Equal(id.PublicKey, testPublicKey) {
-		t.Fatalf("Wrong Public Key %s \n%s", id.PublicKey, testPublicKey)
+		t.Fatalf("wrong public key: %s %s", id.PublicKey, testPublicKey)
 	}
 	if id.Address != testAddr {
-		t.Fatal("Wrong Address")
+		t.Fatalf("wrong address: %s", id.Address)
 	}
 }
 func TestIDString(t *testing.T) {
 	if id.String() != "ID{PublicKey: [49 50 51 52 53 54 55 56 57 48 49 50 51 52 53 54 55 56 57 48 49 50 51 52 53 54 55 56 57 48 49 50], Address: localhost:12345}" {
-		t.Fatalf("String() error: %s", id.String())
+		t.Fatalf("string() error: %s", id.String())
 	}
 }
 func TestIDEquals(t *testing.T) {
 	if !id.Equals(peer.CreateID(testAddr, testPublicKey)) {
-		t.Fatal("Equals() error")
+		t.Fatal("equals() error")
 	}
 }
 func TestIDLess(t *testing.T) {
 
 	if id.Less(peer.CreateID(testAddr, testPublicKey1)) {
-		t.Fatal("Less() error 1")
+		t.Fatal("less() error 1")
 	}
 
 	if !id.Less(peer.CreateID(testAddr, testPublicKey2)) {
-		t.Fatal("Less() error 2")
+		t.Fatal("less() error 2")
 	}
 }
 func TestIDPublicKeyHex(t *testing.T) {
 	if id.PublicKeyHex() != "3132333435363738393031323334353637383930313233343536373839303132" {
-		t.Fatalf("PublicKeyHex() error or hex.EncodeToString() changed defination? value: %v", id.PublicKeyHex())
+		t.Fatalf("publickeyhex() error or hex.encodetostring() changed defination? value: %v", id.PublicKeyHex())
 	}
 }
 func TestIDXor(t *testing.T) {
@@ -57,7 +57,7 @@ func TestIDXor(t *testing.T) {
 	)
 
 	if !comparee.Equals(id.Xor(peer.CreateID(testAddr, testPublicKey2))) {
-		t.Fatalf("Xor() error : \n%v\n%v", comparee, id.Xor(peer.CreateID(testAddr, testPublicKey2)))
+		t.Fatalf("xor() error : %v %v", comparee, id.Xor(peer.CreateID(testAddr, testPublicKey2)))
 	}
 
 }
