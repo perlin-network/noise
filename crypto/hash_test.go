bd21696d9d878fd5eb1c16ad7378382848092701
abbychau <abbychau@gmail.com>
2018-06-26 02:36:24 +0900

    use fatalf and break identity_test

--- crypto/hash_test.go
+++ crypto/hash_test.go
@@ -2,7 +2,6 @@ package crypto
 
 import (
 	"bytes"
-	"fmt"
 	"math/big"
 	"testing"
 )
@@ -14,8 +13,7 @@ func TestHash(t *testing.T) {
 	n, ok := n.SetString("89391711502145780362310349925943903708999319576398061903082165979787487688967", 10)
 	if ok {
 		if n.String() != r.String() {
-			fmt.Printf("%v \n%v \n", r, n)
-			t.Fatal("Hash error")
+			t.Fatalf("Hash error\n%v \n%v \n", r, n)
 		}
 	} else {
 		t.Fatal("Big Int error")
@@ -26,7 +24,6 @@ func TestHashBytes(t *testing.T) {
 	r := HashBytes([]byte("123"))
 	n := []byte{245, 214, 123, 174, 115, 176, 225, 13, 13, 253, 48, 67, 179, 244, 241, 0, 173, 160, 20, 197, 195, 123, 213, 206, 151, 129, 59, 19, 245, 171, 43, 207}
 	if !bytes.Equal(n, r) {
-		fmt.Printf("%v \n%v \n", r, n)
-		t.Fatal("Hash error")
+		t.Fatalf("Hash error : \n%v \n%v \n", r, n)
 	}
 }
--- peer/identity_test.go
+++ peer/identity_test.go
@@ -2,38 +2,40 @@ package peer
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/perlin-network/noise/peer"
 )
 
-func TestID(t *testing.T) {
+var (
+	testPublicKey  = []byte("12345678901234567890123456789012")
+	testPublicKey1 = []byte("12345678901234567890123456789011")
+	testPublicKey2 = []byte("12345678901234567890123456789013")
+	testAddr       = "localhost:12345"
 
-	testPublicKey := []byte("12345678901234567890123456789012")
-	testPublicKey1 := []byte("12345678901234567890123456789011")
-	testPublicKey2 := []byte("12345678901234567890123456789013")
-	testAddr := "localhost:12345"
+	id = peer.CreateID(testAddr, testPublicKey)
+)
 
-	id := peer.CreateID(testAddr, testPublicKey)
+func TestIDEqual(t *testing.T) {
 
 	if !bytes.Equal(id.PublicKey, testPublicKey) {
-		fmt.Printf("%s \n%s", id.PublicKey, testPublicKey)
-		t.Fatal("Wrong Public Key")
+		t.Fatalf("Wrong Public Key %s \n%s", id.PublicKey, testPublicKey)
 	}
-
 	if id.Address != testAddr {
 		t.Fatal("Wrong Address")
 	}
-
+}
+func TestIDString(t *testing.T) {
 	if id.String() != "ID{PublicKey: [49 50 51 52 53 54 55 56 57 48 49 50 51 52 53 54 55 56 57 48 49 50 51 52 53 54 55 56 57 48 49 50], Address: localhost:12345}" {
-		fmt.Printf(id.String())
-		t.Fatal("String() error")
+		t.Fatalf("String() error: %s", id.String())
 	}
-
+}
+func TestIDEquals(t *testing.T) {
 	if !id.Equals(peer.CreateID(testAddr, testPublicKey)) {
 		t.Fatal("Equals() error")
 	}
+}
+func TestIDLess(t *testing.T) {
 
 	if id.Less(peer.CreateID(testAddr, testPublicKey1)) {
 		t.Fatal("Less() error 1")
@@ -42,20 +44,20 @@ func TestID(t *testing.T) {
 	if !id.Less(peer.CreateID(testAddr, testPublicKey2)) {
 		t.Fatal("Less() error 2")
 	}
-
+}
+func TestIDPublicKeyHex(t *testing.T) {
 	if id.PublicKeyHex() != "3132333435363738393031323334353637383930313233343536373839303132" {
-		fmt.Print(id.PublicKeyHex())
-		t.Fatal("PublicKeyHex() error or hex.EncodeToString() changed defination?")
+		t.Fatalf("PublicKeyHex() error or hex.EncodeToString() changed defination? value: %v", id.PublicKeyHex())
 	}
-
+}
+func TestIDXor(t *testing.T) {
 	comparee := peer.CreateID(
 		testAddr,
 		[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	)
 
 	if !comparee.Equals(id.Xor(peer.CreateID(testAddr, testPublicKey2))) {
-		fmt.Printf("%v\n%v", comparee, id.Xor(peer.CreateID(testAddr, testPublicKey2)))
-		t.Fatal("Xor() error")
+		t.Fatalf("Xor() error : \n%v\n%v", comparee, id.Xor(peer.CreateID(testAddr, testPublicKey2)))
 	}
 
 }
