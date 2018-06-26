051b3e0c5ab726fc32e9c2af51d05d2fb879d696
losfair <zhy20000919@hotmail.com>
2018-06-26 11:33:40 +0800

    network: Generic maps

---
+++ network/maps.go
@@ -0,0 +1,40 @@
+//go:generate genny -in=$GOFILE -out=gen-string-PeerClient-$GOFILE gen "Key=string Value=*PeerClient"
+//go:generate genny -in=$GOFILE -out=gen-string-MessageProcessor-$GOFILE gen "Key=string Value=MessageProcessor"
+//go:generate genny -in=$GOFILE -out=gen-uint64-MessageChan-$GOFILE gen "Key=uint64 Value=MessageChan"
+
+package network
+
+import (
+	"sync"
+	"github.com/cheekybits/genny/generic"
+)
+
+type Key generic.Type
+type Value generic.Type
+
+type KeyValueSyncMap struct {
+	inner sync.Map
+}
+
+func (m *KeyValueSyncMap) Store(k Key, v Value) {
+	m.inner.Store(k, v)
+}
+
+func (m *KeyValueSyncMap) Load(k Key) (Value, bool) {
+	val, ok := m.inner.Load(k)
+	if !ok {
+		return nil, false
+	}
+
+	return val.(Value), true
+}
+
+func (m *KeyValueSyncMap) Range(cb func(Key, Value) bool) {
+	m.inner.Range(func (k interface{}, v interface{}) bool {
+		return cb(k.(Key), v.(Value))
+	})
+}
+
+func (m *KeyValueSyncMap) Delete(key Key) {
+	m.inner.Delete(key)
+}
