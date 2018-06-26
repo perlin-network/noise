623c4eb90c1cefd849bcc0ebecf3baebe5110cc4
doug <39219081+doug-perlin@users.noreply.github.com>
2018-06-26 15:04:23 +0800

    Clean up some variables, fixed up some messages

--- examples/basic/setup_test.go
+++ examples/basic/setup_test.go
@@ -5,24 +5,24 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/golang/glog"
 	"github.com/perlin-network/noise/examples/basic/messages"
 )
 
+// ExampleSetupClusters - example of how to use SetupClusters() to automate tests
 func ExampleSetupClusters() {
 	// parse to flags to silence the glog library
 	flag.Parse()
 
 	host := "localhost"
-	cluster1StartPort := 5000
-	cluster1NumPorts := 3
+	startPort := 5000
+	numNodes := 3
 	nodes := []*ClusterNode{}
 	peers := []string{}
 
-	for i := 0; i < cluster1NumPorts; i++ {
+	for i := 0; i < numNodes; i++ {
 		node := &ClusterNode{}
 		node.Host = host
-		node.Port = cluster1StartPort + i
+		node.Port = startPort + i
 
 		nodes = append(nodes, node)
 		peers = append(peers, fmt.Sprintf("%s:%d", node.Host, node.Port))
@@ -34,38 +34,42 @@ func ExampleSetupClusters() {
 
 	if err := SetupCluster(nodes); err != nil {
 		fmt.Print(err)
+		return
 	}
 
 	for i, node := range nodes {
 		if node.Net == nil {
-			fmt.Printf("Expected %d nodes, but node %d is missing a network", len(nodes), i)
+			fmt.Printf("expected %d nodes, but node %d is missing a network", len(nodes), i)
+			return
 		}
 	}
 
 	// check if you can send a message from node 1 and will it be received only in node 2,3
-	{
-		testMessage := "message from node 0"
 
-		// Broadcast is an asynchronous call to send a message to other nodes
-		nodes[0].Net.Broadcast(&messages.BasicMessage{Message: testMessage})
+	testMessage := "message from node 0"
 
-		// Simplificiation: message broadcasting is asynchronous, so need the messages to settle
-		time.Sleep(1 * time.Second)
+	// Broadcast is an asynchronous call to send a message to other nodes
+	nodes[0].Net.Broadcast(&messages.BasicMessage{Message: testMessage})
 
-		if result := nodes[0].PopMessage(); result != nil {
-			glog.Errorf("Expected nothing in node 0, got %v", result)
-		}
-		for i := 1; i < len(nodes); i++ {
-			if result := nodes[i].PopMessage(); result == nil {
-				fmt.Printf("Expected a message in node %d but it was blank", i)
-			} else {
-				if result.Message != testMessage {
-					fmt.Printf("Expected message %s in node %d but got %v", testMessage, i, result)
-				}
+	// Simplificiation: message broadcasting is asynchronous, so need the messages to settle
+	time.Sleep(1 * time.Second)
+
+	if result := nodes[0].PopMessage(); result != nil {
+		fmt.Printf("expected nothing in node 0, got %v", result)
+		return
+	}
+	for i := 1; i < len(nodes); i++ {
+		if result := nodes[i].PopMessage(); result == nil {
+			fmt.Printf("expected a message in node %d but it was blank", i)
+			return
+		} else {
+			if result.Message != testMessage {
+				fmt.Printf("expected message %s in node %d but got %v", testMessage, i, result)
+				return
 			}
 		}
 	}
 
-	fmt.Printf("Success")
-	// Output: Success
+	fmt.Printf("success")
+	// Output: success
 }
