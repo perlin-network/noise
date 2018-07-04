d0cc8aedb62bd3176ad3f944d7e1cbb6d526c858
losfair <zhy20000919@hotmail.com>
2018-07-04 22:59:02 +0800

    crypto: Add signing test

---
+++ crypto/tests/signing_test.go
@@ -0,0 +1,44 @@
+package tests
+
+import (
+	"crypto/rand"
+	"testing"
+	"github.com/perlin-network/noise/crypto"
+	"github.com/perlin-network/noise/crypto/signing/ed25519"
+	"github.com/perlin-network/noise/crypto/hashing/blake2b"
+)
+
+func TestSignVerify(t *testing.T) {
+	sp := ed25519.New()
+	hp := blake2b.New()
+
+	kp := ed25519.RandomKeyPair()
+	message := make([]byte, 120)
+
+	_, err := rand.Read(message)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sig, err := kp.Sign(sp, hp, message)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Test if signature works.
+	ok := crypto.Verify(sp, hp, kp.PublicKey, message, sig)
+	if !ok {
+		t.Fatal("signature verification failed with correct info")
+	}
+
+	ok = crypto.Verify(sp, hp, message, message, sig)
+	if ok {
+		t.Fatal("signature verification failed with wrong public key size/contents")
+	}
+
+	sig[0] = ^sig[0]
+	ok = crypto.Verify(sp, hp, kp.PublicKey, message, sig)
+	if ok {
+		t.Fatal("invalid signature passed verification unexpectedly")
+	}
+}
