9570582ea82bd037ea475adf445c9bfa97882663
doug <39219081+doug-perlin@users.noreply.github.com>
2018-07-04 10:21:22 +0800

    backoff: made backoff use a map

--- network/backoff/plugin.go
+++ network/backoff/plugin.go
@@ -12,53 +12,64 @@ type Plugin struct {
 	*network.Plugin
 
 	sync.Mutex
-	isActive bool
-	backoff  *Backoff
+	backoffs map[string]*Backoff
 }
 
 var PluginID = (*Plugin)(nil)
 
 func (p *Plugin) Startup(net *network.Network) {
-	if p.isActive {
-		p.isActive = false
+	if p.backoffs == nil {
+		p.backoffs = map[string]*Backoff{}
 	}
 }
 
 func (p *Plugin) Receive(ctx *network.MessageContext) error {
-	// no op
+	addr := ctx.Sender().Address
+	if _, exists := p.backoffs[addr]; exists {
+		// if there is an active backoff, clear it
+		delete(p.backoffs, addr)
+	}
 	return nil
 }
 
-func (p *Plugin) Cleanup(net *network.Network) {
-	p.isActive = false
-}
-
 func (p *Plugin) PeerDisconnect(client *network.PeerClient) {
-	// Delete peer if in routing table.
+	addr := client.Address
 
-	if p.isActive {
+	if _, exists := p.backoffs[addr]; exists {
+		// don't activate if it already active
+		glog.Infof("backing done already active\n")
 		return
 	}
 	go func() {
+		time.Sleep(1000 * time.Millisecond)
 		// reset the backoff counter
-		p.backoff = DefaultBackoff()
-		p.isActive = true
+		p.backoffs[addr] = DefaultBackoff()
 		for {
-			if !p.isActive {
+			b, active := p.backoffs[addr]
+			if !active {
+				glog.Infof("backing done already ended\n")
 				break
 			}
-			if p.backoff.TimeoutExceeded() {
-				p.isActive = false
+			glog.Infof("backing off: addr=%s b=%v\n", addr, b)
+			if b.TimeoutExceeded() {
+				glog.Infof("backing done timeout exceeded for add=%s\n", addr)
+				delete(p.backoffs, addr)
 				break
 			}
-			if _, err := client.Network.Dial(client.Address); err != nil {
-				d := p.backoff.NextDuration()
-				glog.Infof("%s, reconnecting in %s", err, d)
+			peer, err := client.Network.Dial(client.Address)
+			if err != nil {
+				d := b.NextDuration()
+				glog.Infof("%s, reconnecting to %s in %s", err, addr, d)
 				time.Sleep(d)
 				continue
 			}
+			if peer == nil {
+				glog.Infof("backing error, peer was nil for addr=%s\n", addr)
+				continue
+			}
+			glog.Infof("backing done successfully reconnected to %s\n", addr)
 			// success
-			p.isActive = false
+			delete(p.backoffs, addr)
 		}
 	}()
 }
