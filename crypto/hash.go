3a78d7328b36edffef87532ac373a4cc8385a735
losfair <zhy20000919@hotmail.com>
2018-07-04 22:58:30 +0800

    crypto/hash: Remove HashBytes

--- crypto/hash.go
+++ crypto/hash.go
@@ -2,15 +2,8 @@ package crypto
 
 import (
 	"math/big"
-
-	"golang.org/x/crypto/blake2b"
 )
 
-func Hash(s *big.Int) *big.Int {
-	return s.SetBytes(HashBytes(s.Bytes()))
-}
-
-func HashBytes(bytes []byte) []byte {
-	result := blake2b.Sum256(bytes)
-	return result[:]
+func Hash(hp HashPolicy, s *big.Int) *big.Int {
+	return s.SetBytes(hp.HashBytes(s.Bytes()))
 }
