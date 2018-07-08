e7dda5a0ce1bd0dce9d317026ac980aba7116473
losfair <zhy20000919@hotmail.com>
2018-07-08 13:40:53 +0800

    crypto/policy: Add NonePolicy

--- crypto/policy.go
+++ crypto/policy.go
@@ -1,5 +1,9 @@
 package crypto
 
+import (
+	"crypto/rand"
+)
+
 type HashPolicy interface {
 	HashBytes(b []byte) []byte
 }
@@ -10,4 +14,44 @@ type SignaturePolicy interface {
 	PrivateToPublic(privateKey []byte) ([]byte, error)
 	Sign(privateKey []byte, message []byte) []byte
 	Verify(publicKey []byte, message []byte, signature []byte) bool
+}
+
+func GenerateKeyPairForNonePolicy() *KeyPair {
+	kp := &KeyPair {
+		PrivateKey: []byte{},
+		PublicKey: make([]byte, 32),
+	}
+
+	_, err := rand.Read(kp.PublicKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return kp
+}
+
+type NonePolicy struct{}
+
+func (p *NonePolicy) HashBytes(b []byte) []byte {
+	return nil
+}
+
+func (p *NonePolicy) PrivateKeySize() int {
+	return 0
+}
+
+func (p *NonePolicy) PublicKeySize() int {
+	return 32
+}
+
+func (p *NonePolicy) PrivateToPublic(privateKey []byte) ([]byte, error) {
+	panic("PrivateToPublic not supported on NonePolicy")
+}
+
+func (p *NonePolicy) Sign(privateKey []byte, message []byte) []byte {
+	return []byte{42} // An empty byte slice becomes nil when unmarshaled by the receiver
+}
+
+func (p *NonePolicy) Verify(publicKey []byte, message []byte, signature []byte) bool {
+	return true
 }
