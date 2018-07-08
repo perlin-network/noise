e3ce26e43f64e54b93695c5b04e8948230d7fa67
losfair <zhy20000919@hotmail.com>
2018-07-08 12:22:33 +0800

    network/utils_test: Test serializeMessageInfoForSigning

--- network/utils_test.go
+++ network/utils_test.go
@@ -1,9 +1,55 @@
 package network
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
+	"crypto/rand"
+
+	"github.com/perlin-network/noise/peer"
+	"github.com/perlin-network/noise/protobuf"
 )
+
+func TestSerializeMessageInfoForSigning(t *testing.T) {
+	mustReadRand := func(size int) []byte {
+		out := make([]byte, size)
+		_, err := rand.Read(out)
+		if err != nil {
+			panic(err)
+		}
+		return out
+	}
+
+	pk1, pk2 := mustReadRand(32), mustReadRand(32)
+
+	ids := []protobuf.ID {
+		protobuf.ID(peer.CreateID("tcp://127.0.0.1:3001", pk1)),
+		protobuf.ID(peer.CreateID("tcp://127.0.0.1:3001", pk2)),
+		protobuf.ID(peer.CreateID("tcp://127.0.0.1:3002", pk1)),
+		protobuf.ID(peer.CreateID("tcp://127.0.0.1:3002", pk2)),
+	}
+
+	messages := [][]byte {
+		[]byte("hello"),
+		[]byte("world"),
+	}
+
+	outputs := make([][]byte, 0)
+
+	for _, id := range ids {
+		for _, msg := range messages {
+			outputs = append(outputs, serializeMessageInfoForSigning(&id, msg))
+		}
+	}
+
+	for i := 0; i < len(outputs); i++ {
+		for j := i + 1; j < len(outputs); j++ {
+			if bytes.Equal(outputs[i], outputs[j]) {
+				t.Fatal("Different inputs produced the same output")
+			}
+		}
+	}
+}
 
 func TestFilterPeers(t *testing.T) {
 	result := FilterPeers("tcp://10.0.0.3:3000", []string{
