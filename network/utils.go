21aa055cb03dedb72edda166e32fcc0afd0c2001
losfair <zhy20000919@hotmail.com>
2018-07-08 12:22:25 +0800

    network/utils: Add serializeMessageInfoForSigning

--- network/utils.go
+++ network/utils.go
@@ -1,5 +1,38 @@
 package network
 
+import (
+	"encoding/binary"
+	"github.com/perlin-network/noise/protobuf"
+)
+
+func serializeMessageInfoForSigning(id *protobuf.ID, message []byte) []byte {
+	const UINT32_SIZE = 4
+
+	serialized := make([]byte, UINT32_SIZE + len(id.Address) + UINT32_SIZE + len(id.PublicKey) + len(message))
+	pos := 0
+
+	binary.LittleEndian.PutUint32(serialized[pos:], uint32(len(id.Address)))
+	pos += UINT32_SIZE
+
+	copy(serialized[pos:], []byte(id.Address))
+	pos += len(id.Address)
+
+	binary.LittleEndian.PutUint32(serialized[pos:], uint32(len(id.PublicKey)))
+	pos += UINT32_SIZE
+
+	copy(serialized[pos:], id.PublicKey)
+	pos += len(id.PublicKey)
+
+	copy(serialized[pos:], message)
+	pos += len(message)
+
+	if pos != len(serialized) {
+		panic("internal error: invalid serialization output")
+	}
+
+	return serialized
+}
+
 // FilterPeers filters out duplicate/empty addresses.
 func FilterPeers(address string, peers []string) (filtered []string) {
 	visited := make(map[string]struct{})
