1b192e4310fd5ddfe02c2eb73f099e538e9e9fdf
Jack Li <jack@thejackli.com>
2018-07-10 19:30:56 -0400

    protobuf: Increase coverage again

--- examples/chat/messages/chat.pb.go
+++ examples/chat/messages/chat.pb.go
@@ -15,6 +15,10 @@ package messages
 import proto "github.com/gogo/protobuf/proto"
 import fmt "fmt"
 import math "math"
+import _ "github.com/gogo/protobuf/gogoproto"
+
+import strings "strings"
+import reflect "reflect"
 
 import io "io"
 
@@ -34,7 +38,6 @@ type ChatMessage struct {
 }
 
 func (m *ChatMessage) Reset()                    { *m = ChatMessage{} }
-func (m *ChatMessage) String() string            { return proto.CompactTextString(m) }
 func (*ChatMessage) ProtoMessage()               {}
 func (*ChatMessage) Descriptor() ([]byte, []int) { return fileDescriptorChat, []int{0} }
 
@@ -47,6 +50,78 @@ func (m *ChatMessage) GetMessage() string {
 
 func init() {
 	proto.RegisterType((*ChatMessage)(nil), "messages.ChatMessage")
+}
+func (this *ChatMessage) VerboseEqual(that interface{}) error {
+	if that == nil {
+		if this == nil {
+			return nil
+		}
+		return fmt.Errorf("that == nil && this != nil")
+	}
+
+	that1, ok := that.(*ChatMessage)
+	if !ok {
+		that2, ok := that.(ChatMessage)
+		if ok {
+			that1 = &that2
+		} else {
+			return fmt.Errorf("that is not of type *ChatMessage")
+		}
+	}
+	if that1 == nil {
+		if this == nil {
+			return nil
+		}
+		return fmt.Errorf("that is type *ChatMessage but is nil && this != nil")
+	} else if this == nil {
+		return fmt.Errorf("that is type *ChatMessage but is not nil && this == nil")
+	}
+	if this.Message != that1.Message {
+		return fmt.Errorf("Message this(%v) Not Equal that(%v)", this.Message, that1.Message)
+	}
+	return nil
+}
+func (this *ChatMessage) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	that1, ok := that.(*ChatMessage)
+	if !ok {
+		that2, ok := that.(ChatMessage)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return this == nil
+	} else if this == nil {
+		return false
+	}
+	if this.Message != that1.Message {
+		return false
+	}
+	return true
+}
+func (this *ChatMessage) GoString() string {
+	if this == nil {
+		return "nil"
+	}
+	s := make([]string, 0, 5)
+	s = append(s, "&messages.ChatMessage{")
+	s = append(s, "Message: "+fmt.Sprintf("%#v", this.Message)+",\n")
+	s = append(s, "}")
+	return strings.Join(s, "")
+}
+func valueToGoStringChat(v interface{}, typ string) string {
+	rv := reflect.ValueOf(v)
+	if rv.IsNil() {
+		return "nil"
+	}
+	pv := reflect.Indirect(rv).Interface()
+	return fmt.Sprintf("func(v %v) *%v { return &v } ( %#v )", typ, typ, pv)
 }
 func (m *ChatMessage) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
@@ -81,6 +156,86 @@ func encodeVarintChat(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return offset + 1
 }
+func NewPopulatedChatMessage(r randyChat, easy bool) *ChatMessage {
+	this := &ChatMessage{}
+	this.Message = string(randStringChat(r))
+	if !easy && r.Intn(10) != 0 {
+	}
+	return this
+}
+
+type randyChat interface {
+	Float32() float32
+	Float64() float64
+	Int63() int64
+	Int31() int32
+	Uint32() uint32
+	Intn(n int) int
+}
+
+func randUTF8RuneChat(r randyChat) rune {
+	ru := r.Intn(62)
+	if ru < 10 {
+		return rune(ru + 48)
+	} else if ru < 36 {
+		return rune(ru + 55)
+	}
+	return rune(ru + 61)
+}
+func randStringChat(r randyChat) string {
+	v1 := r.Intn(100)
+	tmps := make([]rune, v1)
+	for i := 0; i < v1; i++ {
+		tmps[i] = randUTF8RuneChat(r)
+	}
+	return string(tmps)
+}
+func randUnrecognizedChat(r randyChat, maxFieldNumber int) (dAtA []byte) {
+	l := r.Intn(5)
+	for i := 0; i < l; i++ {
+		wire := r.Intn(4)
+		if wire == 3 {
+			wire = 5
+		}
+		fieldNumber := maxFieldNumber + r.Intn(100)
+		dAtA = randFieldChat(dAtA, r, fieldNumber, wire)
+	}
+	return dAtA
+}
+func randFieldChat(dAtA []byte, r randyChat, fieldNumber int, wire int) []byte {
+	key := uint32(fieldNumber)<<3 | uint32(wire)
+	switch wire {
+	case 0:
+		dAtA = encodeVarintPopulateChat(dAtA, uint64(key))
+		v2 := r.Int63()
+		if r.Intn(2) == 0 {
+			v2 *= -1
+		}
+		dAtA = encodeVarintPopulateChat(dAtA, uint64(v2))
+	case 1:
+		dAtA = encodeVarintPopulateChat(dAtA, uint64(key))
+		dAtA = append(dAtA, byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)))
+	case 2:
+		dAtA = encodeVarintPopulateChat(dAtA, uint64(key))
+		ll := r.Intn(100)
+		dAtA = encodeVarintPopulateChat(dAtA, uint64(ll))
+		for j := 0; j < ll; j++ {
+			dAtA = append(dAtA, byte(r.Intn(256)))
+		}
+	default:
+		dAtA = encodeVarintPopulateChat(dAtA, uint64(key))
+		dAtA = append(dAtA, byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)))
+	}
+	return dAtA
+}
+func encodeVarintPopulateChat(dAtA []byte, v uint64) []byte {
+	for v >= 1<<7 {
+		dAtA = append(dAtA, uint8(uint64(v)&0x7f|0x80))
+		v >>= 7
+	}
+	dAtA = append(dAtA, uint8(v))
+	return dAtA
+}
 func (m *ChatMessage) Size() (n int) {
 	var l int
 	_ = l
@@ -103,6 +258,24 @@ func sovChat(x uint64) (n int) {
 }
 func sozChat(x uint64) (n int) {
 	return sovChat(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (this *ChatMessage) String() string {
+	if this == nil {
+		return "nil"
+	}
+	s := strings.Join([]string{`&ChatMessage{`,
+		`Message:` + fmt.Sprintf("%v", this.Message) + `,`,
+		`}`,
+	}, "")
+	return s
+}
+func valueToStringChat(v interface{}) string {
+	rv := reflect.ValueOf(v)
+	if rv.IsNil() {
+		return "nil"
+	}
+	pv := reflect.Indirect(rv).Interface()
+	return fmt.Sprintf("*%v", pv)
 }
 func (m *ChatMessage) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
@@ -291,12 +464,15 @@ var (
 func init() { proto.RegisterFile("examples/chat/messages/chat.proto", fileDescriptorChat) }
 
 var fileDescriptorChat = []byte{
-	// 107 bytes of a gzipped FileDescriptorProto
+	// 157 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x4c, 0xad, 0x48, 0xcc,
 	0x2d, 0xc8, 0x49, 0x2d, 0xd6, 0x4f, 0xce, 0x48, 0x2c, 0xd1, 0xcf, 0x4d, 0x2d, 0x2e, 0x4e, 0x4c,
-	0x87, 0xf2, 0xf4, 0x0a, 0x8a, 0xf2, 0x4b, 0xf2, 0x85, 0x38, 0x60, 0x82, 0x4a, 0xea, 0x5c, 0xdc,
-	0xce, 0x19, 0x89, 0x25, 0xbe, 0x10, 0xbe, 0x90, 0x04, 0x17, 0x3b, 0x54, 0x4a, 0x82, 0x51, 0x81,
-	0x51, 0x83, 0x33, 0x08, 0xc6, 0x75, 0x12, 0x38, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6,
-	0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0x48, 0x62, 0x03, 0x9b, 0x65, 0x0c, 0x08, 0x00,
-	0x00, 0xff, 0xff, 0xc1, 0xce, 0x0a, 0x65, 0x70, 0x00, 0x00, 0x00,
+	0x87, 0xf2, 0xf4, 0x0a, 0x8a, 0xf2, 0x4b, 0xf2, 0x85, 0x38, 0x60, 0x82, 0x52, 0x4a, 0xe9, 0xf9,
+	0xe9, 0xf9, 0xfa, 0x60, 0xd1, 0xa4, 0xd2, 0x34, 0x7d, 0x10, 0x0f, 0xcc, 0x01, 0xb3, 0x20, 0xaa,
+	0x95, 0xd4, 0xb9, 0xb8, 0x9d, 0x33, 0x12, 0x4b, 0x7c, 0x21, 0x7a, 0x84, 0x24, 0xb8, 0xd8, 0xa1,
+	0xda, 0x25, 0x18, 0x15, 0x18, 0x35, 0x38, 0x83, 0x60, 0x5c, 0x27, 0x9d, 0x1b, 0x0f, 0xe5, 0x18,
+	0x1e, 0x3c, 0x94, 0x63, 0xfc, 0xf0, 0x50, 0x8e, 0xf1, 0xc7, 0x43, 0x39, 0xc6, 0x86, 0x47, 0x72,
+	0x8c, 0x2b, 0x1e, 0xc9, 0x31, 0xee, 0x78, 0x24, 0xc7, 0x78, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47,
+	0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c, 0xc7, 0x90, 0xc4, 0x06, 0x36, 0xdd, 0x18,
+	0x10, 0x00, 0x00, 0xff, 0xff, 0xc9, 0xd0, 0x80, 0xdb, 0xb0, 0x00, 0x00, 0x00,
 }
--- examples/cluster_benchmark/messages/bench.pb.go
+++ examples/cluster_benchmark/messages/bench.pb.go
@@ -15,6 +15,10 @@ package messages
 import proto "github.com/gogo/protobuf/proto"
 import fmt "fmt"
 import math "math"
+import _ "github.com/gogo/protobuf/gogoproto"
+
+import strings "strings"
+import reflect "reflect"
 
 import io "io"
 
@@ -33,12 +37,76 @@ type Empty struct {
 }
 
 func (m *Empty) Reset()                    { *m = Empty{} }
-func (m *Empty) String() string            { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()               {}
 func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptorBench, []int{0} }
 
 func init() {
 	proto.RegisterType((*Empty)(nil), "messages.Empty")
+}
+func (this *Empty) VerboseEqual(that interface{}) error {
+	if that == nil {
+		if this == nil {
+			return nil
+		}
+		return fmt.Errorf("that == nil && this != nil")
+	}
+
+	that1, ok := that.(*Empty)
+	if !ok {
+		that2, ok := that.(Empty)
+		if ok {
+			that1 = &that2
+		} else {
+			return fmt.Errorf("that is not of type *Empty")
+		}
+	}
+	if that1 == nil {
+		if this == nil {
+			return nil
+		}
+		return fmt.Errorf("that is type *Empty but is nil && this != nil")
+	} else if this == nil {
+		return fmt.Errorf("that is type *Empty but is not nil && this == nil")
+	}
+	return nil
+}
+func (this *Empty) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	that1, ok := that.(*Empty)
+	if !ok {
+		that2, ok := that.(Empty)
+		if ok {
+			that1 = &that2
+		} else {
+			return false
+		}
+	}
+	if that1 == nil {
+		return this == nil
+	} else if this == nil {
+		return false
+	}
+	return true
+}
+func (this *Empty) GoString() string {
+	if this == nil {
+		return "nil"
+	}
+	s := make([]string, 0, 4)
+	s = append(s, "&messages.Empty{")
+	s = append(s, "}")
+	return strings.Join(s, "")
+}
+func valueToGoStringBench(v interface{}, typ string) string {
+	rv := reflect.ValueOf(v)
+	if rv.IsNil() {
+		return "nil"
+	}
+	pv := reflect.Indirect(rv).Interface()
+	return fmt.Sprintf("func(v %v) *%v { return &v } ( %#v )", typ, typ, pv)
 }
 func (m *Empty) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
@@ -67,6 +135,85 @@ func encodeVarintBench(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return offset + 1
 }
+func NewPopulatedEmpty(r randyBench, easy bool) *Empty {
+	this := &Empty{}
+	if !easy && r.Intn(10) != 0 {
+	}
+	return this
+}
+
+type randyBench interface {
+	Float32() float32
+	Float64() float64
+	Int63() int64
+	Int31() int32
+	Uint32() uint32
+	Intn(n int) int
+}
+
+func randUTF8RuneBench(r randyBench) rune {
+	ru := r.Intn(62)
+	if ru < 10 {
+		return rune(ru + 48)
+	} else if ru < 36 {
+		return rune(ru + 55)
+	}
+	return rune(ru + 61)
+}
+func randStringBench(r randyBench) string {
+	v1 := r.Intn(100)
+	tmps := make([]rune, v1)
+	for i := 0; i < v1; i++ {
+		tmps[i] = randUTF8RuneBench(r)
+	}
+	return string(tmps)
+}
+func randUnrecognizedBench(r randyBench, maxFieldNumber int) (dAtA []byte) {
+	l := r.Intn(5)
+	for i := 0; i < l; i++ {
+		wire := r.Intn(4)
+		if wire == 3 {
+			wire = 5
+		}
+		fieldNumber := maxFieldNumber + r.Intn(100)
+		dAtA = randFieldBench(dAtA, r, fieldNumber, wire)
+	}
+	return dAtA
+}
+func randFieldBench(dAtA []byte, r randyBench, fieldNumber int, wire int) []byte {
+	key := uint32(fieldNumber)<<3 | uint32(wire)
+	switch wire {
+	case 0:
+		dAtA = encodeVarintPopulateBench(dAtA, uint64(key))
+		v2 := r.Int63()
+		if r.Intn(2) == 0 {
+			v2 *= -1
+		}
+		dAtA = encodeVarintPopulateBench(dAtA, uint64(v2))
+	case 1:
+		dAtA = encodeVarintPopulateBench(dAtA, uint64(key))
+		dAtA = append(dAtA, byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)))
+	case 2:
+		dAtA = encodeVarintPopulateBench(dAtA, uint64(key))
+		ll := r.Intn(100)
+		dAtA = encodeVarintPopulateBench(dAtA, uint64(ll))
+		for j := 0; j < ll; j++ {
+			dAtA = append(dAtA, byte(r.Intn(256)))
+		}
+	default:
+		dAtA = encodeVarintPopulateBench(dAtA, uint64(key))
+		dAtA = append(dAtA, byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)), byte(r.Intn(256)))
+	}
+	return dAtA
+}
+func encodeVarintPopulateBench(dAtA []byte, v uint64) []byte {
+	for v >= 1<<7 {
+		dAtA = append(dAtA, uint8(uint64(v)&0x7f|0x80))
+		v >>= 7
+	}
+	dAtA = append(dAtA, uint8(v))
+	return dAtA
+}
 func (m *Empty) Size() (n int) {
 	var l int
 	_ = l
@@ -85,6 +232,23 @@ func sovBench(x uint64) (n int) {
 }
 func sozBench(x uint64) (n int) {
 	return sovBench(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (this *Empty) String() string {
+	if this == nil {
+		return "nil"
+	}
+	s := strings.Join([]string{`&Empty{`,
+		`}`,
+	}, "")
+	return s
+}
+func valueToStringBench(v interface{}) string {
+	rv := reflect.ValueOf(v)
+	if rv.IsNil() {
+		return "nil"
+	}
+	pv := reflect.Indirect(rv).Interface()
+	return fmt.Sprintf("*%v", pv)
 }
 func (m *Empty) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
@@ -246,12 +410,15 @@ func init() {
 }
 
 var fileDescriptorBench = []byte{
-	// 107 bytes of a gzipped FileDescriptorProto
+	// 157 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x4f, 0xad, 0x48, 0xcc,
 	0x2d, 0xc8, 0x49, 0x2d, 0xd6, 0x4f, 0xce, 0x29, 0x2d, 0x2e, 0x49, 0x2d, 0x8a, 0x4f, 0x4a, 0xcd,
 	0x4b, 0xce, 0xc8, 0x4d, 0x2c, 0xca, 0xd6, 0xcf, 0x4d, 0x2d, 0x2e, 0x4e, 0x4c, 0x4f, 0x2d, 0xd6,
-	0x07, 0x0b, 0xe9, 0x15, 0x14, 0xe5, 0x97, 0xe4, 0x0b, 0x71, 0xc0, 0x44, 0x95, 0xd8, 0xb9, 0x58,
-	0x5d, 0x73, 0x0b, 0x4a, 0x2a, 0x9d, 0x04, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1,
-	0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x92, 0xd8, 0xc0, 0x6a, 0x8d, 0x01, 0x01, 0x00,
-	0x00, 0xff, 0xff, 0x75, 0x0b, 0x44, 0x23, 0x5e, 0x00, 0x00, 0x00,
+	0x07, 0x0b, 0xe9, 0x15, 0x14, 0xe5, 0x97, 0xe4, 0x0b, 0x71, 0xc0, 0x44, 0xa5, 0x94, 0xd2, 0xf3,
+	0xd3, 0xf3, 0xf5, 0xc1, 0xa2, 0x49, 0xa5, 0x69, 0xfa, 0x20, 0x1e, 0x98, 0x03, 0x66, 0x41, 0x54,
+	0x2b, 0xb1, 0x73, 0xb1, 0xba, 0xe6, 0x16, 0x94, 0x54, 0x3a, 0xe9, 0xdc, 0x78, 0x28, 0xc7, 0xf0,
+	0xe0, 0xa1, 0x1c, 0xe3, 0x87, 0x87, 0x72, 0x8c, 0x3f, 0x1e, 0xca, 0x31, 0x36, 0x3c, 0x92, 0x63,
+	0x5c, 0xf1, 0x48, 0x8e, 0x71, 0xc7, 0x23, 0x39, 0xc6, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92,
+	0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x71, 0xc2, 0x63, 0x39, 0x86, 0x24, 0x36, 0xb0, 0x6e, 0x63, 0x40,
+	0x00, 0x00, 0x00, 0xff, 0xff, 0x37, 0xd7, 0x78, 0xdd, 0x9e, 0x00, 0x00, 0x00,
 }
