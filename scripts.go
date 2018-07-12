09866d9a6f627381ef88d77cfb8451b9ff0be945
Jack Li <jack@thejackli.com>
2018-07-12 17:20:41 -0400

    scripts: use filepath.Join

--- scripts.go
+++ scripts.go
@@ -30,10 +30,10 @@ func generateProtos(dir string) error {
 		if filepath.Ext(path) == ".proto" {
 			// args
 			args := []string{
-				"-I=.",
-				fmt.Sprintf("-I=%s/src", goPath),
-				fmt.Sprintf("-I=%s/src/github.com/gogo/protobuf/protobuf", goPath),
-				fmt.Sprintf("--proto_path=%s/src/github.com", goPath),
+                "-I=.",
+				fmt.Sprintf("-I=%s", filepath.Join(goPath, "src"),
+				fmt.Sprintf("-I=%s", filepath.Join(goPath, "src", "github.com", "gogo", "protobuf", "protobuf")),
+				fmt.Sprintf("--proto_path=%s", filepath.Join(goPath, "src", "github.com")),
 				"--gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:.",
 				path,
 			}
