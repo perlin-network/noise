19bf8c194bfbffa89e67c3e32e62737bd8926752
Jack Li <jack@thejackli.com>
2018-07-12 17:21:01 -0400

    scripts: fix error

--- scripts.go
+++ scripts.go
@@ -30,8 +30,8 @@ func generateProtos(dir string) error {
 		if filepath.Ext(path) == ".proto" {
 			// args
 			args := []string{
-                "-I=.",
-				fmt.Sprintf("-I=%s", filepath.Join(goPath, "src"),
+				"-I=.",
+				fmt.Sprintf("-I=%s", filepath.Join(goPath, "src")),
 				fmt.Sprintf("-I=%s", filepath.Join(goPath, "src", "github.com", "gogo", "protobuf", "protobuf")),
 				fmt.Sprintf("--proto_path=%s", filepath.Join(goPath, "src", "github.com")),
 				"--gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:.",
