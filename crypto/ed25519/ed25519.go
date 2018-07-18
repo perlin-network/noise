ebbd7948578a8c28c033845a5ab0ddd246e891e7
Kenta Iwasaki <kenta@perlin.net>
2018-07-18 19:32:52 +0800

    ed25519: Imported SUPERCOP-referenced AMD64-optimized implementation for Ed25519 ECC operations by @gbot.

--- crypto/ed25519/ed25519.go
+++ crypto/ed25519/ed25519.go
@@ -4,8 +4,6 @@ import (
 	"crypto/rand"
 
 	"github.com/perlin-network/noise/crypto"
-
-	ed25519lib "golang.org/x/crypto/ed25519"
 )
 
 // Ed25519 represents the ed25519 cryptographic signature scheme.
@@ -23,7 +21,7 @@ func New() *Ed25519 {
 
 // GenerateKeys generates a private and public key using the ed25519 signature scheme.
 func (p *Ed25519) GenerateKeys() ([]byte, []byte, error) {
-	publicKey, privateKey, err := ed25519lib.GenerateKey(rand.Reader)
+	publicKey, privateKey, err := GenerateKey(rand.Reader)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -32,17 +30,17 @@ func (p *Ed25519) GenerateKeys() ([]byte, []byte, error) {
 
 // PrivateKeySize returns the private key length.
 func (p *Ed25519) PrivateKeySize() int {
-	return ed25519lib.PrivateKeySize
+	return PrivateKeySize
 }
 
 // PrivateToPublic returns the public key given the private key.
 func (p *Ed25519) PrivateToPublic(privateKey []byte) ([]byte, error) {
-	return ([]byte)(ed25519lib.PrivateKey(privateKey).Public().(ed25519lib.PublicKey)), nil
+	return ([]byte)(PrivateKey(privateKey).Public().(PublicKey)), nil
 }
 
 // PublicKeySize returns the public key length.
 func (p *Ed25519) PublicKeySize() int {
-	return ed25519lib.PublicKeySize
+	return PublicKeySize
 }
 
 // RandomKeyPair generates a randomly seeded ed25519 key pair.
@@ -52,23 +50,23 @@ func (p *Ed25519) RandomKeyPair() *crypto.KeyPair {
 
 // Sign returns an ed25519-signed message given an private key and message.
 func (p *Ed25519) Sign(privateKey []byte, message []byte) []byte {
-	if len(privateKey) != ed25519lib.PrivateKeySize {
+	if len(privateKey) != PrivateKeySize {
 		return make([]byte, 0)
 	}
-	return ed25519lib.Sign(ed25519lib.PrivateKey(privateKey), message)
+	return Sign(PrivateKey(privateKey), message)
 }
 
 // Verify returns true if the  signature was signed using the given public key and message.
 func (p *Ed25519) Verify(publicKey []byte, message []byte, signature []byte) bool {
-	if len(publicKey) != ed25519lib.PublicKeySize {
+	if len(publicKey) != PublicKeySize {
 		return false
 	}
-	return ed25519lib.Verify(publicKey, message, signature)
+	return Verify(publicKey, message, signature)
 }
 
 // RandomKeyPair generates a randomly seeded ed25519 key pair.
 func RandomKeyPair() *crypto.KeyPair {
-	publicKey, privateKey, err := ed25519lib.GenerateKey(rand.Reader)
+	publicKey, privateKey, err := GenerateKey(rand.Reader)
 	if err != nil {
 		panic(err)
 	}
