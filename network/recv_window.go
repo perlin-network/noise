8379b62ff3d8a317194d96fe99a0685ad7d5cc50
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-08-05 06:44:52 +0200

    network: use recv window

--- network/recv_window.go
+++ network/recv_window.go
@@ -2,68 +2,56 @@ package network
 
 import (
 	"sync"
-
-	"github.com/pkg/errors"
 )
 
 // RecvWindow represents a window that buffers and cuts off messages based on their priority.
 type RecvWindow struct {
 	sync.Mutex
-
-	size       int
-	buffer     *RingBuffer
-	localNonce uint64
+	size      int
+	lastNonce uint64
+	data      map[uint64]interface{}
 }
 
 // NewRecvWindow creates a new receive buffer window with a specific buffer size.
 func NewRecvWindow(size int) *RecvWindow {
 	return &RecvWindow{
-		size:       size,
-		buffer:     NewRingBuffer(size),
-		localNonce: 1,
+		size:      size,
+		lastNonce: 1,
+		data:      make(map[uint64]interface{}, 0),
 	}
 }
 
+// SetLocalNonce sets a expected nonce.
 func (w *RecvWindow) SetLocalNonce(nonce uint64) {
 	w.Lock()
-	w.localNonce = nonce
+	w.lastNonce = nonce
 	w.Unlock()
 }
 
-// Update pushes messages from the networks receive queue into the buffer.
-func (w *RecvWindow) Update() []interface{} {
-	ready := make([]interface{}, 0)
-
+// Push adds value with a given nonce to the window.
+func (w *RecvWindow) Push(nonce uint64, value interface{}) {
 	w.Lock()
-	i := 0
-	for ; i < w.size; i++ {
-		cursor := w.buffer.Index(i)
-		if *cursor == nil {
-			break
-		}
-		ready = append(ready, *cursor)
-		*cursor = nil
-	}
-	if i > 0 && i < w.size {
-		w.buffer.MoveForward(i)
-	}
-	w.localNonce += uint64(i)
+	w.data[nonce] = value
 	w.Unlock()
-
-	return ready
 }
 
-// Input places a new received message into the receive buffer.
-func (w *RecvWindow) Input(nonce uint64, msg interface{}) error {
+// Pop returns a slice of values from last till not yet received nonce.
+func (w *RecvWindow) Pop() []interface{} {
+	res := make([]interface{}, 0)
+
 	w.Lock()
 	defer w.Unlock()
 
-	offset := int(nonce - w.localNonce)
-
-	if offset < 0 || offset >= w.size {
-		return errors.Errorf("Local nonce is %d while received %d", w.localNonce, nonce)
+	id := w.lastNonce
+	for {
+		val, ok := w.data[id]
+		if !ok {
+			w.lastNonce = id
+			break
+		}
+		res = append(res, val)
+		delete(w.data, id)
+		id++
 	}
-
-	*w.buffer.Index(offset) = msg
-	return nil
+	return res
 }
