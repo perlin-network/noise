4cc1fffdad8d2ea9371b033999b6a9518c09335d
Jack Li <jack@thejackli.com>
2018-10-16 16:42:17 -0400

    test: fix more tests

--- examples/cluster_benchmark/main.go
+++ examples/cluster_benchmark/main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/perlin-network/noise/network"
 	"github.com/perlin-network/noise/network/backoff"
 	"github.com/perlin-network/noise/network/discovery"
+	"github.com/perlin-network/noise/types/opcode"
 )
 
 const MESSAGE_THRESHOLD uint64 = 2000
@@ -96,6 +97,7 @@ func main() {
 	log.Info().Str("private_key", keys.PrivateKeyHex()).Msg("")
 	log.Info().Str("public_key", keys.PublicKeyHex()).Msg("")
 
+	opcode.RegisterMessageType(opcode.Opcode(1000), &messages.Empty{})
 	builder := network.NewBuilder()
 	builder.SetKeys(keys)
 	builder.SetAddress(network.FormatAddress(protocol, host, port))
