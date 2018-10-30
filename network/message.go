b84b95207176d5fb066223a225a6631adc37070d
Jack Li <jack@thejackli.com>
2018-10-30 08:28:20 -0700

    network: swap ctx names

--- network/message.go
+++ network/message.go
@@ -16,31 +16,31 @@ type PluginContext struct {
 }
 
 // Reply sends back a message to an incoming message's incoming stream.
-func (ctx *PluginContext) Reply(gCtx context.Context, message proto.Message) error {
-	return ctx.client.Reply(gCtx, ctx.nonce, message)
+func (pctx *PluginContext) Reply(ctx context.Context, message proto.Message) error {
+	return pctx.client.Reply(ctx, pctx.nonce, message)
 }
 
 // Message returns the decoded protobuf message.
-func (ctx *PluginContext) Message() proto.Message {
-	return ctx.message
+func (pctx *PluginContext) Message() proto.Message {
+	return pctx.message
 }
 
 // Client returns the peer client.
-func (ctx *PluginContext) Client() *PeerClient {
-	return ctx.client
+func (pctx *PluginContext) Client() *PeerClient {
+	return pctx.client
 }
 
 // Network returns the entire node's network.
-func (ctx *PluginContext) Network() *Network {
-	return ctx.client.Network
+func (pctx *PluginContext) Network() *Network {
+	return pctx.client.Network
 }
 
 // Self returns the node's ID.
-func (ctx *PluginContext) Self() peer.ID {
-	return ctx.Network().ID
+func (pctx *PluginContext) Self() peer.ID {
+	return pctx.Network().ID
 }
 
 // Sender returns the peer's ID.
-func (ctx *PluginContext) Sender() peer.ID {
-	return *ctx.client.ID
+func (pctx *PluginContext) Sender() peer.ID {
+	return *pctx.client.ID
 }
