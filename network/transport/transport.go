e2ccb49ed4726e099a43cfcfc6202a028849a43a
losfair <zhy20000919@hotmail.com>
2018-11-04 19:39:48 +0800

    network: Cleanup

--- network/transport/transport.go
+++
@@ -1,9 +0,0 @@
-package transport
-
-import "net"
-
-// Layer represents a transport protocol layer.
-type Layer interface {
-	Listen(port int) (net.Listener, error)
-	Dial(address string) (net.Conn, error)
-}
