20d9f05cb811fc03605ab91c2f2551f6d4e617ae
losfair <zhy20000919@hotmail.com>
2018-11-05 00:29:18 +0800

    network: Add writeUint16.

--- network/intutils.go
+++ network/intutils.go
@@ -8,3 +8,9 @@ func writeUvarint(buf *[]byte, x uint64) {
 	n := binary.PutUvarint((*buf)[endPos:], x)
 	*buf = (*buf)[:endPos+n]
 }
+
+func writeUint16(buf *[]byte, x uint16) {
+	endPos := len(*buf)
+	*buf = append(*buf, make([]byte, 2)...)
+	binary.LittleEndian.PutUint16((*buf)[endPos:], x)
+}
