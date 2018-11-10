5923efa61e3dd4358ee1ecd2c31f83b2560c5ac5
losfair <zhy20000919@hotmail.com>
2018-11-10 11:50:51 +0800

    protocol/adapter: Add comments for the adapter interfaces.

--- protocol/adapter.go
+++ protocol/adapter.go
@@ -2,11 +2,13 @@ package protocol
 
 type RecvMessageCallback func(message []byte)
 
+// A ConnectionAdapter is an adapter that establishes real/virtual connections (message adapters), both passively and actively.
 type ConnectionAdapter interface {
 	EstablishPassively(c *Controller, local []byte) chan MessageAdapter
 	EstablishActively(c *Controller, local []byte, remote []byte) (MessageAdapter, error)
 }
 
+// A MessageAdapter is an adapter that sends/receives messages, usually corresponding to a real/virtual connection.
 type MessageAdapter interface {
 	RemoteEndpoint() []byte
 	StartRecvMessage(c *Controller, callback RecvMessageCallback)
@@ -14,6 +16,7 @@ type MessageAdapter interface {
 	Close()
 }
 
+// An IdentityAdapter is an adapter that provides identity-related operations like signing and verification.
 type IdentityAdapter interface {
 	MyIdentity() []byte
 	Sign(input []byte) []byte
