3af9ceab8572424822dd4085c879b09d688eddf0
losfair <zhy20000919@hotmail.com>
2018-11-10 20:17:47 +0800

    connection: Generalize TCPConnectionAdapter to AddressableConnectionAdapter & implement publicly visible address exchange.

--- connection/connection.go
+++ connection/connection.go
@@ -11,46 +11,50 @@ import (
 	"io"
 	"net"
 	"sync"
-	"time"
 )
 
-var _ protocol.ConnectionAdapter = (*TCPConnectionAdapter)(nil)
+const MaxPublicVisibleAddressCandidates = 8
 
-type TCPConnectionAdapter struct {
+var _ protocol.ConnectionAdapter = (*AddressableConnectionAdapter)(nil)
+
+type Dialer func(address string) (net.Conn, error)
+
+type AddressableConnectionAdapter struct {
 	listener    net.Listener
-	listenAddr  string
-	dialTimeout time.Duration
+	dialer      Dialer
 	idToAddress sync.Map
+
+	reportedPubliclyVisibleAddresses      []*PubliclyVisibleAddress
+	reportedPubliclyVisibleAddressesMutex sync.Mutex
 }
 
-type TCPMessageAdapter struct {
+type PubliclyVisibleAddress struct {
+	address string
+	count   uint64
+}
+
+type AddressableMessageAdapter struct {
 	conn              net.Conn
 	local             []byte
 	remote            []byte
 	finalizerNotifier chan struct{}
 }
 
-func StartTCPConnectionAdapter(
-	listenAddr string,
-	dialTimeout time.Duration,
-) (*TCPConnectionAdapter, error) {
-	listener, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		return nil, err
-	}
-
-	return &TCPConnectionAdapter{
-		listener:    listener,
-		listenAddr:  listenAddr,
-		dialTimeout: dialTimeout,
+func StartAddressableConnectionAdapter(
+	listener net.Listener,
+	dialer Dialer,
+) (*AddressableConnectionAdapter, error) {
+	return &AddressableConnectionAdapter{
+		listener: listener,
+		dialer:   dialer,
 	}, nil
 }
 
-func (a *TCPConnectionAdapter) MapIDToAddress(id []byte, addr string) {
+func (a *AddressableConnectionAdapter) MapIDToAddress(id []byte, addr string) {
 	a.idToAddress.Store(string(id), addr)
 }
 
-func (a *TCPConnectionAdapter) lookupAddressByID(id []byte) (string, error) {
+func (a *AddressableConnectionAdapter) lookupAddressByID(id []byte) (string, error) {
 	if v, ok := a.idToAddress.Load(string(id)); ok {
 		return v.(string), nil
 	} else {
@@ -58,21 +62,21 @@ func (a *TCPConnectionAdapter) lookupAddressByID(id []byte) (string, error) {
 	}
 }
 
-func (a *TCPConnectionAdapter) EstablishActively(c *protocol.Controller, local []byte, remote []byte) (protocol.MessageAdapter, error) {
+func (a *AddressableConnectionAdapter) EstablishActively(c *protocol.Controller, local []byte, remote []byte) (protocol.MessageAdapter, error) {
 	remoteAddr, err := a.lookupAddressByID(remote)
 	if err != nil {
 		return nil, err
 	}
 
-	conn, err := net.DialTimeout("tcp", remoteAddr, a.dialTimeout)
+	conn, err := a.dialer(remoteAddr)
 	if err != nil {
 		return nil, err
 	}
 
-	return startTCPMessageAdapter(conn, local, remote, false)
+	return startAddressableMessageAdapter(a, conn, local, remote, remoteAddr, false)
 }
 
-func (a *TCPConnectionAdapter) EstablishPassively(c *protocol.Controller, local []byte) chan protocol.MessageAdapter {
+func (a *AddressableConnectionAdapter) EstablishPassively(c *protocol.Controller, local []byte) chan protocol.MessageAdapter {
 	ch := make(chan protocol.MessageAdapter)
 	go func() {
 		defer close(ch)
@@ -89,7 +93,7 @@ func (a *TCPConnectionAdapter) EstablishPassively(c *protocol.Controller, local 
 				continue
 			}
 
-			adapter, err := startTCPMessageAdapter(conn, local, nil, true)
+			adapter, err := startAddressableMessageAdapter(a, conn, local, nil, "", true)
 			if err != nil {
 				log.Error().Err(err).Msg("unable to start message adapter")
 				continue
@@ -101,10 +105,56 @@ func (a *TCPConnectionAdapter) EstablishPassively(c *protocol.Controller, local 
 	return ch
 }
 
-func startTCPMessageAdapter(conn net.Conn, local, remote []byte, passive bool) (*TCPMessageAdapter, error) {
+func (a *AddressableConnectionAdapter) getPubliclyVisibleAddress() string {
+	a.reportedPubliclyVisibleAddressesMutex.Lock()
+	var ret string
+	if len(a.reportedPubliclyVisibleAddresses) > 0 {
+		ret = a.reportedPubliclyVisibleAddresses[0].address
+	}
+	a.reportedPubliclyVisibleAddressesMutex.Unlock()
+	return ret
+}
+
+func (a *AddressableConnectionAdapter) updatePubliclyVisibleAddress(address string) {
+	a.reportedPubliclyVisibleAddressesMutex.Lock()
+	defer a.reportedPubliclyVisibleAddressesMutex.Unlock()
+
+	for i, pva := range a.reportedPubliclyVisibleAddresses {
+		if pva.address == address {
+			pva.count++
+			p := i - 1
+			// bubble up
+			for p >= 0 {
+				if a.reportedPubliclyVisibleAddresses[p].count < a.reportedPubliclyVisibleAddresses[p+1].count {
+					t := a.reportedPubliclyVisibleAddresses[p]
+					a.reportedPubliclyVisibleAddresses[p] = a.reportedPubliclyVisibleAddresses[p+1]
+					a.reportedPubliclyVisibleAddresses[p+1] = t
+					p--
+				} else {
+					break
+				}
+			}
+			return
+		}
+	}
+
+	if len(a.reportedPubliclyVisibleAddresses) > MaxPublicVisibleAddressCandidates-1 {
+		a.reportedPubliclyVisibleAddresses = a.reportedPubliclyVisibleAddresses[:MaxPublicVisibleAddressCandidates-1]
+	}
+
+	// always keep the last received publicly visible address in storage
+	// so it will always have a chance of being preferred.
+	a.reportedPubliclyVisibleAddresses = append(a.reportedPubliclyVisibleAddresses, &PubliclyVisibleAddress{
+		address: address,
+		count:   1,
+	})
+}
+
+func startAddressableMessageAdapter(connAdapter *AddressableConnectionAdapter, conn net.Conn, local, remote []byte, remoteAddr string, passive bool) (*AddressableMessageAdapter, error) {
 	if len(local) > 255 || len(remote) > 255 {
 		return nil, errors.New("local or remote id too long")
 	}
+	byteBuf := make([]byte, 1)
 
 	if passive {
 		remote = make([]byte, len(local))
@@ -119,6 +169,25 @@ func startTCPMessageAdapter(conn net.Conn, local, remote []byte, passive bool) (
 		if err != nil {
 			conn.Close()
 			return nil, err
+		}
+
+		_, err = io.ReadFull(conn, byteBuf)
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
+
+		pvaLen := int(byteBuf[0])
+		if pvaLen > 0 {
+			pvaBytes := make([]byte, pvaLen)
+			_, err = io.ReadFull(conn, pvaBytes)
+			if err != nil {
+				conn.Close()
+				return nil, err
+			}
+			pva := string(pvaBytes)
+			connAdapter.updatePubliclyVisibleAddress(pva)
+			log.Debug().Msgf("Current publicly visible address: %s", connAdapter.getPubliclyVisibleAddress())
 		}
 	} else {
 		_, err := conn.Write(local)
@@ -136,9 +205,20 @@ func startTCPMessageAdapter(conn net.Conn, local, remote []byte, passive bool) (
 			conn.Close()
 			return nil, errors.Errorf("inconsistent remotes %s and %s", hex.EncodeToString(recvRemote), hex.EncodeToString(remote))
 		}
+
+		if len(remoteAddr) > 255 {
+			conn.Close()
+			return nil, errors.Errorf("remote address is too long")
+		}
+
+		_, err = conn.Write(append([]byte{byte(len(remoteAddr))}, []byte(remoteAddr)...))
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
 	}
 
-	adapter := &TCPMessageAdapter{
+	adapter := &AddressableMessageAdapter{
 		conn:              conn,
 		local:             local,
 		remote:            remote,
@@ -148,16 +228,16 @@ func startTCPMessageAdapter(conn net.Conn, local, remote []byte, passive bool) (
 	return adapter, nil
 }
 
-func (a *TCPMessageAdapter) Close() {
+func (a *AddressableMessageAdapter) Close() {
 	a.conn.Close()
 	close(a.finalizerNotifier)
 }
 
-func (a *TCPMessageAdapter) RemoteEndpoint() []byte {
+func (a *AddressableMessageAdapter) RemoteEndpoint() []byte {
 	return a.remote
 }
 
-func (a *TCPMessageAdapter) SendMessage(c *protocol.Controller, message []byte) error {
+func (a *AddressableMessageAdapter) SendMessage(c *protocol.Controller, message []byte) error {
 	lenBuf := make([]byte, 16)
 	n := binary.PutUvarint(lenBuf, uint64(len(message)))
 	_, err := a.conn.Write(lenBuf[:n])
@@ -171,7 +251,7 @@ func (a *TCPMessageAdapter) SendMessage(c *protocol.Controller, message []byte) 
 	return nil
 }
 
-func (a *TCPMessageAdapter) StartRecvMessage(c *protocol.Controller, callback protocol.RecvMessageCallback) {
+func (a *AddressableMessageAdapter) StartRecvMessage(c *protocol.Controller, callback protocol.RecvMessageCallback) {
 	go runRecvWorker(a.finalizerNotifier, a.conn, callback)
 }
 
