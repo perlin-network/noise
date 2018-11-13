38f50be70bd24736ff393c7460b46b10ca325b69
doug <39219081+doug-perlin@users.noreply.github.com>
2018-11-13 21:08:07 +0800

    test: fix getting started test

--- examples/getting_started/main.go
+++ examples/getting_started/main.go
@@ -1,62 +1,74 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
+	"fmt"
+	"net"
 	"strings"
+	"time"
 
+	"github.com/perlin-network/noise/connection"
 	"github.com/perlin-network/noise/crypto/ed25519"
+	"github.com/perlin-network/noise/identity"
 	"github.com/perlin-network/noise/log"
+	"github.com/perlin-network/noise/protocol"
 )
 
 func main() {
 	// process flags
 	portFlag := flag.Int("port", 3000, "port to listen to")
 	hostFlag := flag.String("host", "localhost", "host to listen to")
-	protocolFlag := flag.String("protocol", "tcp", "protocol to use (kcp/tcp)")
-	peersFlag := flag.String("peers", "", "peers to connect to")
-	natFlag := flag.Bool("nat", false, "enable nat traversal")
-	reconnectFlag := flag.Bool("reconnect", false, "enable reconnections")
+	peersFlag := flag.String("peers", "", "peers to connect to in format: peerKeyHash1=host1:port1,peerKeyHash2=host2:port2,...")
 	flag.Parse()
 
-	port := uint16(*portFlag)
+	port := *portFlag
 	host := *hostFlag
-	protocol := *protocolFlag
-	natEnabled := *natFlag
-	reconnectEnabled := *reconnectFlag
 	peers := strings.Split(*peersFlag, ",")
 
 	keys := ed25519.RandomKeyPair()
+	idAdapter := identity.NewDefaultIdentityAdapter(keys)
 
 	log.Info().Str("private_key", keys.PrivateKeyHex()).Msg("")
 	log.Info().Str("public_key", keys.PublicKeyHex()).Msg("")
 
-	builder := network.NewBuilder()
-	builder.SetKeys(keys)
-	builder.SetAddress(network.FormatAddress(protocol, host, port))
-
-	// Register NAT traversal plugin.
-	if natEnabled {
-		nat.RegisterPlugin(builder)
-	}
-
-	// Register the reconnection plugin
-	if reconnectEnabled {
-		builder.AddPlugin(new(backoff.Plugin))
-	}
-
-	// Register peer discovery plugin.
-	builder.AddPlugin(new(discovery.Plugin))
-
-	net, err := builder.Build()
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
-		return
 	}
 
-	go net.Listen()
+	connAdapter, err := connection.StartAddressableConnectionAdapter(listener, func(addr string) (net.Conn, error) {
+		return net.DialTimeout("tcp", addr, 10*time.Second)
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("")
+	}
+
+	node := protocol.NewNode(
+		protocol.NewController(),
+		connAdapter,
+		idAdapter,
+	)
+	node.Start()
+
+	node.AddService(42, func(message *protocol.Message) {
+		log.Info().Msgf("received payload from %s: %s", hex.EncodeToString(message.Sender), string(message.Body.Payload))
+	})
 
 	if len(peers) > 0 {
-		net.Bootstrap(peers...)
+		for _, peerKV := range peers {
+			if len(peerKV) == 0 {
+				// this is a blank parameter
+				continue
+			}
+			peer := strings.Split(peerKV, "=")
+			peerID, err := hex.DecodeString(peer[0])
+			if err != nil {
+				log.Fatal().Err(err).Msg("")
+			}
+			remoteAddr := peer[1]
+			connAdapter.MapIDToAddress(peerID, remoteAddr)
+		}
 	}
 
 	select {}
