f81a96b55659f24cc2a2836518fe56958f08131c
Kenta Iwasaki <kenta@perlin.net>
2019-02-15 02:23:52 +0800

    peer: cleaned up test

--- peer_test.go
+++ peer_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"github.com/perlin-network/noise/identity/ed25519"
+	"github.com/perlin-network/noise/log"
 	"github.com/perlin-network/noise/payload"
 	"github.com/perlin-network/noise/transport"
 	"github.com/pkg/errors"
@@ -71,6 +72,9 @@ func TestDecodeMessage(t *testing.T) {
 // 3. Check the callbacks must be called in sequence
 // 4. Check the callbacks must be called exactly once
 func TestPeer(t *testing.T) {
+	log.Disable()
+	defer log.Enable()
+
 	resetOpcodes()
 	opcodeTest := RegisterMessage(NextAvailableOpcode(), (*testMsg)(nil))
 
@@ -87,136 +91,139 @@ func TestPeer(t *testing.T) {
 	layer := transport.NewBuffered()
 
 	go func() {
-		lis, err := layer.Listen(port)
+		listener, err := layer.Listen("127.0.0.1", port)
 		assert.Nil(t, err)
+
 		wgListen.Done()
 
-		lisConn, err := lis.Accept()
-		assert.Nil(t, err, "failed to accept")
+		conn, err := listener.Accept()
+		assert.NoError(t, err)
 
 		wgAccept.Done()
 
-		p := newPeer(nil, nil)
+		peer := newPeer(nil, nil)
+
 		var buf []byte
-		reader := bufio.NewReader(lisConn)
+		reader := bufio.NewReader(conn)
 
-		// read the size
+		// Read message size.
 		size, err := binary.ReadUvarint(reader)
-		assert.Nil(t, err, "failed to read size")
+		assert.NoError(t, err)
 
-		// read the message
+		// Read message.
 		buf = make([]byte, size)
+
 		_, err = io.ReadFull(reader, buf)
-		assert.Nil(t, err, "failed to read message")
-		_, msg, err := p.DecodeMessage(buf)
-		assert.Equal(t, text, msg.(testMsg).text, "invalid text. expected %s, actual %s", text, msg.(testMsg).text)
+		assert.NoError(t, err)
 
-		// create a new message
-		sendMessage, err := p.EncodeMessage(testMsg{text: text})
-		assert.Nil(t, err, "failed to encode message")
-		msgSize := len(sendMessage)
+		_, msg, err := peer.DecodeMessage(buf)
+		assert.Equal(t, text, msg.(testMsg).text)
+
+		// Create a new message.
+		payload, err := peer.EncodeMessage(testMsg{text: text})
+		assert.NoError(t, err)
+
 		buf = make([]byte, binary.MaxVarintLen64)
-		prepended := binary.PutUvarint(buf[:], uint64(msgSize))
-		buf = append(buf[:prepended], sendMessage[:]...)
+		prepended := binary.PutUvarint(buf[:], uint64(len(payload)))
+		buf = append(buf[:prepended], payload[:]...)
 
-		// send the message
-		_, err = lisConn.Write(buf)
-		assert.Nil(t, err, "failed to write message")
+		// Send the message.
+		_, err = conn.Write(buf)
+		assert.NoError(t, err)
 	}()
 
 	wgListen.Wait()
 
-	dialConn, err := layer.Dial(fmt.Sprintf(":%d", port))
-	assert.Nilf(t, err, "Dial error: %v", err)
+	conn, err := layer.Dial(fmt.Sprintf("%s:%d", "127.0.0.1", port))
+	assert.NoError(t, err)
 
 	wgAccept.Wait()
 
 	var state int32 = 0
 
-	p := getPeer(t, layer, dialConn, port)
+	p := peer(t, layer, conn, port)
 
 	p.OnEncodeHeader(func(node *Node, peer *Peer, header, msg []byte) (bytes []byte, e error) {
-		checkState(t, &state, 0)
+		check(t, &state, 0)
 		return nil, nil
 	})
 
 	p.OnEncodeFooter(func(node *Node, peer *Peer, header, msg []byte) (bytes []byte, e error) {
-		checkState(t, &state, 1)
+		check(t, &state, 1)
 		return nil, nil
 	})
 
 	p.BeforeMessageSent(func(node *Node, peer *Peer, msg []byte) (bytes []byte, e error) {
-		checkState(t, &state, 2)
+		check(t, &state, 2)
 		return msg, nil
 	})
 
 	p.AfterMessageSent(func(node *Node, peer *Peer) error {
-		checkState(t, &state, 3)
+		check(t, &state, 3)
 		return nil
 	})
 
 	p.BeforeMessageReceived(func(node *Node, peer *Peer, msg []byte) (bytes []byte, e error) {
-		checkState(t, &state, 4)
+		check(t, &state, 4)
 		return msg, nil
 	})
 
 	p.OnDecodeHeader(func(node *Node, peer *Peer, reader payload.Reader) error {
-		checkState(t, &state, 5)
+		check(t, &state, 5)
 		return nil
 	})
 
 	p.OnDecodeFooter(func(node *Node, peer *Peer, msg []byte, reader payload.Reader) error {
-		checkState(t, &state, 6)
+		check(t, &state, 6)
 		return nil
 	})
 
 	p.AfterMessageReceived(func(node *Node, peer *Peer) error {
-		checkState(t, &state, 7)
+		check(t, &state, 7)
 		return nil
 	})
 
 	p.OnConnError(func(node *Node, peer *Peer, err error) error {
-		checkState(t, &state, 9)
+		check(t, &state, 11)
 		return nil
 	})
 
 	var wgDisconnect sync.WaitGroup
 	wgDisconnect.Add(1)
+
 	p.OnDisconnect(func(node *Node, peer *Peer) error {
 		defer wgDisconnect.Done()
 
-		checkState(t, &state, 10)
+		check(t, &state, 9)
 		return nil
 	})
 
 	p.init()
 
-	// send a message
+	// Send a message.
 	err = p.SendMessage(testMsg{text: text})
-	assert.Nil(t, err, "Failed to send message")
+	assert.NoError(t, err)
 
-	// read a message
+	// Read a message.
 	msg := <-p.Receive(opcodeTest)
-	assert.Equal(t, text, msg.(testMsg).text, "invalid text. expected %s, actual %s", text, msg.(testMsg).text)
+	assert.Equal(t, text, msg.(testMsg).text)
 
-	checkState(t, &state, 8)
+	check(t, &state, 8)
 
 	p.Disconnect()
 
 	wgDisconnect.Wait()
 
-	checkState(t, &state, 11)
+	check(t, &state, 10)
 }
 
 // check the state equal to the expected state, and then increment it by 1
-func checkState(t *testing.T, state *int32, expectedState int32) {
-	s := atomic.LoadInt32(state)
-	assert.True(t, s == expectedState, "invalid state in OnEncodeHeader: expected %d, actual %d", expectedState, s)
-
-	atomic.AddInt32(state, 1)
+func check(t *testing.T, currentState *int32, expectedState int32) {
+	assert.Equal(t, atomic.LoadInt32(currentState), expectedState)
+	atomic.AddInt32(currentState, 1)
 }
 
-func getPeer(t *testing.T, layer transport.Layer, conn net.Conn, port uint16) *Peer {
+func peer(t *testing.T, layer transport.Layer, conn net.Conn, port uint16) *Peer {
 	params := DefaultParams()
 	params.ID = ed25519.Random()
 	params.Port = port
