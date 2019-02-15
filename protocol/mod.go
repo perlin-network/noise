7e9e8fabcc5229f4442d75035970c6197a9816ce
losfair <zhy20000919@hotmail.com>
2019-02-15 13:55:29 +0800

    protocol: Remove blocksMutex.

--- protocol/mod.go
+++ protocol/mod.go
@@ -4,7 +4,7 @@ import (
 	"github.com/perlin-network/noise"
 	"github.com/perlin-network/noise/log"
 	"github.com/pkg/errors"
-	"sync"
+	"sync/atomic"
 )
 
 const (
@@ -23,8 +23,8 @@ type Block interface {
 }
 
 type Protocol struct {
-	blocks      []Block
-	blocksMutex sync.Mutex
+	blocks       []Block
+	blocksSealed uint32
 }
 
 func New() *Protocol {
@@ -33,28 +33,25 @@ func New() *Protocol {
 
 // Register registers a block to this protocol sequentially.
 func (p *Protocol) Register(blk Block) {
-	p.blocksMutex.Lock()
+	// This is not a strict check. Only here to help users find their mistakes.
+	if atomic.LoadUint32(&p.blocksSealed) == 1 {
+		panic("Register() cannot be called after Enforce().")
+	}
 	p.blocks = append(p.blocks, blk)
-	p.blocksMutex.Unlock()
 }
 
 // Enforce enforces that all peers of a node follow the given protocol.
 func (p *Protocol) Enforce(node *noise.Node) {
-	// TODO(kenta): attempt to remove mutices
+	atomic.StoreUint32(&p.blocksSealed, 1)
 
-	p.blocksMutex.Lock()
 	for _, block := range p.blocks {
 		block.OnRegister(p, node)
 	}
-	p.blocksMutex.Unlock()
 
 	node.OnPeerInit(func(node *noise.Node, peer *noise.Peer) error {
 		go func() {
 			peer.OnDisconnect(func(node *noise.Node, peer *noise.Peer) error {
 				blockIndex := peer.LoadOrStore(KeyProtocolCurrentBlockIndex, 0).(int)
-
-				p.blocksMutex.Lock()
-				defer p.blocksMutex.Unlock()
 
 				if blockIndex >= len(p.blocks) {
 					return nil
@@ -70,9 +67,7 @@ func (p *Protocol) Enforce(node *noise.Node) {
 					return
 				}
 
-				p.blocksMutex.Lock()
 				err := p.blocks[blockIndex].OnBegin(p, peer)
-				p.blocksMutex.Unlock()
 
 				if err != nil {
 					log.Warn().Err(err).Msg("Received an error following protocol.")
