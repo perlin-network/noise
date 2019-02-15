3a833d964884012421476d2f5f02bc0f1fb16c49
Ahmad Muzakkir <ahmad@perlin.net>
2019-02-15 11:00:46 +0800

    msg/test: move the tests from peer_test.go into msg_test.go

--- msg_test.go
+++ msg_test.go
@@ -1,1 +1,38 @@
 package noise
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEncodeMessage(t *testing.T) {
+	resetOpcodes()
+	o := RegisterMessage(Opcode(123), (*testMsg)(nil))
+
+	msg := testMsg{
+		text: "hello",
+	}
+
+	p := newPeer(nil, nil)
+
+	bytes, err := p.EncodeMessage(msg)
+	assert.Nil(t, err)
+	assert.Equal(t, append([]byte{byte(o)}, msg.Write()...), bytes)
+}
+
+func TestDecodeMessage(t *testing.T) {
+	resetOpcodes()
+	o := RegisterMessage(Opcode(45), (*testMsg)(nil))
+
+	msg := testMsg{
+		text: "world",
+	}
+	assert.Equal(t, o, RegisterMessage(o, (*testMsg)(nil)))
+
+	p := newPeer(nil, nil)
+
+	resultO, resultM, err := p.DecodeMessage(append([]byte{byte(o)}, msg.Write()...))
+	assert.Nil(t, err)
+	assert.Equal(t, o, resultO)
+	assert.Equal(t, msg, resultM)
+}
--- peer_test.go
+++ peer_test.go
@@ -34,38 +34,6 @@ func (m testMsg) Write() []byte {
 	return payload.NewWriter(nil).WriteString(m.text).Bytes()
 }
 
-func TestEncodeMessage(t *testing.T) {
-	resetOpcodes()
-	o := RegisterMessage(Opcode(123), (*testMsg)(nil))
-
-	msg := testMsg{
-		text: "hello",
-	}
-
-	p := newPeer(nil, nil)
-
-	bytes, err := p.EncodeMessage(msg)
-	assert.Nil(t, err)
-	assert.Equal(t, append([]byte{byte(o)}, msg.Write()...), bytes)
-}
-
-func TestDecodeMessage(t *testing.T) {
-	resetOpcodes()
-	o := RegisterMessage(Opcode(45), (*testMsg)(nil))
-
-	msg := testMsg{
-		text: "world",
-	}
-	assert.Equal(t, o, RegisterMessage(o, (*testMsg)(nil)))
-
-	p := newPeer(nil, nil)
-
-	resultO, resultM, err := p.DecodeMessage(append([]byte{byte(o)}, msg.Write()...))
-	assert.Nil(t, err)
-	assert.Equal(t, o, resultO)
-	assert.Equal(t, msg, resultM)
-}
-
 // What this test does:
 // 1. Check send message
 // 2. Check receive message
