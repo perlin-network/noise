d5a7c4592e57d3bc02a27531ffd8614fd6746bbd
Kenta Iwasaki <kenta@perlin.net>
2019-04-03 07:03:13 +0800

    xnoise/error: ignore standard errors

--- xnoise/error.go
+++ xnoise/error.go
@@ -3,12 +3,16 @@ package xnoise
 import (
 	"github.com/perlin-network/noise"
 	"github.com/pkg/errors"
+	"io"
 	"log"
 )
 
 func LogErrors(ctx noise.Context) error {
 	ctx.Peer().InterceptErrors(func(err error) {
 		switch errors.Cause(err) {
+		case noise.ErrDisconnect:
+		case io.ErrClosedPipe:
+		case io.EOF:
 		default:
 			log.Printf("%+v\n", err)
 		}
