fa8f1d288f42adc8be6ecfdce652f7d03a8671b7
Kenta Iwasaki <kenta@perlin.net>
2020-01-29 19:34:28 +0800

    keys: add docs

--- keys.go
+++ keys.go
@@ -8,20 +8,31 @@ import (
 )
 
 const (
-	PublicKeySize  = ed25519.PublicKeySize
+	// Size in bytes of a peers public key.
+	PublicKeySize = ed25519.PublicKeySize
+
+	// Size in bytes of a peers private key.
 	PrivateKeySize = ed25519.PrivateKeySize
 )
 
 type (
-	PublicKey  [PublicKeySize]byte
+	// Default peer public key type.
+	PublicKey [PublicKeySize]byte
+
+	// Default peer private key type.
 	PrivateKey [PrivateKeySize]byte
 )
 
 var (
-	ZeroPublicKey  PublicKey
+	// Zero-value for a peer public key.
+	ZeroPublicKey PublicKey
+
+	// Zero-value for a peer private key.
 	ZeroPrivateKey PrivateKey
 )
 
+// GenerateKeys randomly generates a new pair of cryptographic keys. Nil may be passed to rand in order to use
+// crypto/rand by default. It throws an error if rand is invalid.
 func GenerateKeys(rand io.Reader) (publicKey PublicKey, privateKey PrivateKey, err error) {
 	pub, priv, err := ed25519.GenerateKey(rand)
 	if err != nil {
@@ -34,26 +45,32 @@ func GenerateKeys(rand io.Reader) (publicKey PublicKey, privateKey PrivateKey, e
 	return publicKey, privateKey, nil
 }
 
+// Verify returns true if the cryptographic signature of data is representative of this public key.
 func (k PublicKey) Verify(data, signature []byte) bool {
 	return ed25519.Verify(k[:], data, signature)
 }
 
+// String returns the hexadecimal representation of this public key.
 func (k PublicKey) String() string {
 	return hex.EncodeToString(k[:])
 }
 
+// MarshalJSON returns the hexadecimal representation of this public key in JSON. It should never throw an error.
 func (k PublicKey) MarshalJSON() ([]byte, error) {
-	return json.Marshal(hex.EncodeToString(k[:]))
+	return json.Marshal(k.String())
 }
 
+// Sign uses this private key to sign data and return a cryptographic signature as a slice of bytes.
 func (k PrivateKey) Sign(data []byte) []byte {
 	return ed25519.Sign(k[:], data)
 }
 
+// String returns the hexadecimal representation of this private key.
 func (k PrivateKey) String() string {
 	return hex.EncodeToString(k[:])
 }
 
+// MarshalJSON returns the hexadecimal representation of this private key in JSON. It should never throw an error.
 func (k PrivateKey) MarshalJSON() ([]byte, error) {
 	return json.Marshal(hex.EncodeToString(k[:]))
 }
