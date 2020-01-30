06ecbb910a8ebea26c1d027c46e7f246583e4d82
Kenta Iwasaki <kenta@perlin.net>
2020-01-30 11:16:01 +0800

    cmd/chat: cleanup chat example

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -15,6 +15,12 @@ import (
 	"time"
 )
 
+var (
+	hostFlag    = pflag.IPP("host", "h", nil, "binding host")
+	portFlag    = pflag.Uint16P("port", "p", 0, "binding port")
+	addressFlag = pflag.StringP("address", "a", "", "publicly reachable network address")
+)
+
 type chatMessage struct {
 	contents string
 }
@@ -27,23 +33,22 @@ func unmarshalChatMessage(buf []byte) (chatMessage, error) {
 	return chatMessage{contents: strings.ToValidUTF8(string(buf), "")}, nil
 }
 
-var (
-	hostFlag    = pflag.IPP("host", "h", nil, "binding host")
-	portFlag    = pflag.Uint16P("port", "p", 0, "binding port")
-	addressFlag = pflag.StringP("address", "a", "", "publicly reachable network address")
-)
-
+// check panics if err is not nil.
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// printedLength is the total prefix length of a public key associated to a chat users ID.
 const printedLength = 8
 
+// An example chat application on Noise.
 func main() {
+	// Parse flags/options.
 	pflag.Parse()
 
+	// Create a new configured node.
 	node, err := noise.NewNode(
 		noise.WithNodeBindHost(*hostFlag),
 		noise.WithNodeBindPort(*portFlag),
@@ -51,53 +56,121 @@ func main() {
 	)
 	check(err)
 
+	// Release resources associated to node at the end of the program/
 	defer node.Close()
 
+	// Register the chatMessage Go type to the node with an associated unmarshal function.
 	node.RegisterMessage(chatMessage{}, unmarshalChatMessage)
 
-	node.Handle(func(ctx noise.HandlerContext) error {
-		if ctx.IsRequest() {
-			return nil
-		}
+	// Register a message handler to the node.
+	node.Handle(handle)
 
-		obj, err := ctx.DecodeMessage()
-		if err != nil {
-			return nil
-		}
-
-		msg, ok := obj.(chatMessage)
-		if !ok {
-			return nil
-		}
-
-		fmt.Printf("%s(%s)> %s\n", ctx.ID().Address, ctx.ID().ID.String()[:printedLength], msg.contents)
-
-		return nil
-	})
-
+	// Bind Kademlia to the node.
 	overlay := kademlia.NewProtocol()
 	node.Bind(overlay)
 
+	// Have the node start listening for new peers.
 	check(node.Listen())
 
-	fmt.Printf("Your ID is %s(%s). Type '/discover' to attempt to discover new peers, or '/peers' to list"+
-		" out all peers you are connected to.\n", node.ID().Address, node.ID().ID.String()[:printedLength])
+	// Print out the nodes ID and a help message comprised of commands.
+	help(node)
 
-	peers := make([]*noise.Client, 0, pflag.NArg())
+	// Ping nodes to initially bootstrap and discover peers from.
+	bootstrap(node, pflag.Args()...)
 
-	for _, peerAddr := range pflag.Args() {
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		peer, err := node.Ping(ctx, peerAddr)
-		cancel()
+	// Attempt to discover peers if we are bootstrapped to any nodes.
+	discover(overlay)
 
+	// Accept chat message inputs and handle chat commands in a separate goroutine.
+	go input(func(line string) {
+		chat(node, overlay, line)
+	})
+
+	// Wait until Ctrl+C or a termination call is done.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	// Close stdin to kill the input goroutine.
+	check(os.Stdin.Close())
+
+	// Empty println.
+	println()
+}
+
+// input handles inputs from stdin.
+func input(callback func(string)) {
+	r := bufio.NewReader(os.Stdin)
+
+	for {
+		buf, _, err := r.ReadLine()
 		if err != nil {
-			fmt.Printf("Failed to ping bootstrap node (%s). Skipping... [error: %s]\n", peerAddr, err)
+			if errors.Is(err, io.EOF) {
+				return
+			}
+
+			check(err)
+		}
+
+		line := string(buf)
+		if len(line) == 0 {
 			continue
 		}
 
-		peers = append(peers, peer)
+		callback(line)
+	}
+}
+
+// handle handles and prints out valid chat messages from peers.
+func handle(ctx noise.HandlerContext) error {
+	if ctx.IsRequest() {
+		return nil
 	}
 
+	obj, err := ctx.DecodeMessage()
+	if err != nil {
+		return nil
+	}
+
+	msg, ok := obj.(chatMessage)
+	if !ok {
+		return nil
+	}
+
+	if len(msg.contents) == 0 {
+		return nil
+	}
+
+	fmt.Printf("%s(%s)> %s\n", ctx.ID().Address, ctx.ID().ID.String()[:printedLength], msg.contents)
+
+	return nil
+}
+
+// help prints out the users ID and commands available.
+func help(node *noise.Node) {
+	fmt.Printf("Your ID is %s(%s). Type '/discover' to attempt to discover new "+
+		"peers, or '/peers' to list out all peers you are connected to.\n",
+		node.ID().Address,
+		node.ID().ID.String()[:printedLength],
+	)
+}
+
+// bootstrap pings and dials an array of network addresses which we may interact with and  discover peers from.
+func bootstrap(node *noise.Node, addresses ...string) {
+	for _, addr := range addresses {
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		_, err := node.Ping(ctx, addr)
+		cancel()
+
+		if err != nil {
+			fmt.Printf("Failed to ping bootstrap node (%s). Skipping... [error: %s]\n", addr, err)
+			continue
+		}
+	}
+}
+
+// discover uses Kademlia to discover new peers from nodes we already are aware of.
+func discover(overlay *kademlia.Protocol) {
 	ids := overlay.Discover()
 
 	var str []string
@@ -110,87 +183,49 @@ func main() {
 	} else {
 		fmt.Printf("Did not discover any peers.\n")
 	}
+}
 
-	go func() {
-		r := bufio.NewReader(os.Stdin)
+// peers prints out all peers we are already aware of.
+func peers(overlay *kademlia.Protocol) {
+	ids := overlay.Table().Peers()
 
-		for {
-			buf, _, err := r.ReadLine()
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					return
-				}
+	var str []string
+	for _, id := range ids {
+		str = append(str, fmt.Sprintf("%s(%s)", id.Address, id.ID.String()[:printedLength]))
+	}
 
-				check(err)
-			}
+	fmt.Printf("You know %d peer(s): [%v]\n", len(ids), strings.Join(str, ", "))
+}
 
-			line := string(buf)
+// chat handles sending chat messages and handling chat commands.
+func chat(node *noise.Node, overlay *kademlia.Protocol, line string) {
+	switch line {
+	case "/discover":
+		discover(overlay)
+		return
+	case "/peers":
+		peers(overlay)
+		return
+	default:
+	}
 
-			if len(line) == 0 {
-				continue
-			}
+	if strings.HasPrefix(line, "/") {
+		help(node)
+		return
+	}
 
-			switch line {
-			case "/discover":
-				ids := overlay.Discover()
+	for _, id := range overlay.Table().Peers() {
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		err := node.SendMessage(ctx, id.Address, chatMessage{contents: line})
+		cancel()
 
-				var str []string
-				for _, id := range ids {
-					str = append(str, fmt.Sprintf("%s(%s)", id.Address, id.ID.String()[:printedLength]))
-				}
-
-				if len(ids) > 0 {
-					fmt.Printf("Discovered %d peer(s): [%v]\n", len(ids), strings.Join(str, ", "))
-				} else {
-					fmt.Printf("Did not discover any peers.\n")
-				}
-
-				continue
-			case "/peers":
-				ids := overlay.Table().Peers()
-
-				var str []string
-				for _, id := range ids {
-					str = append(str, fmt.Sprintf("%s(%s)", id.Address, id.ID.String()[:printedLength]))
-				}
-
-				fmt.Printf("You know %d peer(s): [%v]\n", len(ids), strings.Join(str, ", "))
-
-				continue
-			default:
-			}
-
-			if strings.HasPrefix(line, "/") {
-				fmt.Printf("Your ID is %s(%s). Type '/discover' to attempt to discover new "+
-					"peers, or '/peers' to list out all peers you are connected to.\n",
-					node.ID().Address,
-					node.ID().ID.String()[:printedLength],
-				)
-
-				continue
-			}
-
-			for _, id := range overlay.Table().Peers() {
-				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-				err := node.SendMessage(ctx, id.Address, chatMessage{contents: line})
-				cancel()
-
-				if err != nil {
-					fmt.Printf("Failed to send message to %s(%s). Skipping... [error: %s]\n",
-						id.Address,
-						id.ID.String()[:printedLength],
-						err,
-					)
-					continue
-				}
-			}
+		if err != nil {
+			fmt.Printf("Failed to send message to %s(%s). Skipping... [error: %s]\n",
+				id.Address,
+				id.ID.String()[:printedLength],
+				err,
+			)
+			continue
 		}
-	}()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
-
-	check(os.Stdin.Close())
-	println()
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412
 	github.com/oasislabs/ed25519 v0.0.0-20191122104632-9d9ffc15f526
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.4.0
 	github.com/valyala/bytebufferpool v1.0.0
 	go.uber.org/atomic v1.5.1
